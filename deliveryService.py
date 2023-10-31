18f24b9515f082b2a2f92ce3cd4860766133039d
ujeong <ohyoujung90@gmail.com>
2023-10-31 21:36:15 +0900

    add Sub Class

--- deliveryService.py
+++ deliveryService.py
@@ -1,8 +1,9 @@
 # 클래스 자율과제 >> 편의점 반값 택배 접수
 
 import random
-from datetime import date
-import datetime
+# import datetime
+# from datetime import timedelta
+from datetime import datetime
 
 
 # 택배 접수 클래스 생성
@@ -17,6 +18,9 @@ class Delivery:
         self.t_address = t_address
         self.name = name            # 물품명
         
+        # print(f'{self.name} 물품이 접수되었습니다.')
+        
+        # 예약번호, 운송장번호
         res_num = random.randint(0,9999999999999) # 예약번호(13자리)
         deli_num = random.randint(0,999999999999) # 운송장번호(12자리)
         
@@ -28,9 +32,9 @@ class Delivery:
         self.deli_fee = 0
         
         # 접수 날짜 표시(yyyy-mm-dd)
-        # self.day = datetime.datetime.now()      >> 시간까지 출력됨
-        today = date.today()                    # >> 오늘 날짜 반환
-        self.resDay = date(today.year, today.month, 26)  # >> 접수 날짜: 2023-10-26
+        # self.resDay = datetime.today()
+        # self.resDay = datetime(2023, 10, 13, 21, 40, 5)
+        self.resDay = datetime(2023, 10, 30)    # >> 접수 날짜 2023-10-30 고정
         
 
     # 무게에 따른 배송료 계산
@@ -72,22 +76,66 @@ class Delivery:
         return num
     
     # 배송 현황 조회
-    # def statusCheck(self, today):
-    #     # 3일 이상          -> 고객 전달
-    #     # 1일 이상, 3일 미만 -> 배송 중
-    #     # 1일 미만         -> 배송 대기 중
-    #     # today = date.today()  >>오늘 날짜 리턴
+    def statusCheck(self, today):
+        # 날짜 차이(조회 날짜 - 접수 날짜) 계산
+        # 3일 이상             -> 고객 전달
+        # 1일 이상, 3일 미만    -> 배송 중
+        # 1일 미만            -> 배송 대기 중
         
-    #     condition = abs(today - self.resDay)
-    #     if condition >= 3:
-    #         # print(f'구분: 반값택배\t접수일자: {self.day}\t운송장번호: {self.deli_num}\t상태: 고객전달')
-    #         print("상태: 고객전달")
-    #     elif 1 <= condition < 3:
-    #         print("상태: 배송 중")
-    #     else:
-    #         print("배송 대기 중")
+        # 접수 날짜
+        # self.resDay = datetime(2023, 10, 28)
+        
+        condition = today - self.resDay
+        
+        if condition.days >= 3:
+            print(f'{condition.days}일 소요. 고객 전달')
+        elif 1 <= condition.days:
+            print(f'{condition.days}일 소요. 배송 중')
+        else:
+            print(f'{condition.days}일 소요. 배송 대기 중')
+        
+        return condition.days
             
 
+# GS25
+class GS25(Delivery):
+    def __init__(self, t_name, t_phoneNum, t_address, name, weight, resDay):
+        super().__init__(t_name, t_phoneNum, t_address, name)
+        self.weight = weight
+        self.resDay = resDay
+        self.storeName = 'GS25'
+        
+        print(f'{self.storeName} 에서 {self.resDay} 에 {self.name} 물품을 접수했습니다.')
+        
+    # 무게 
+    def checkWeight(self):
+        super().checkWeight(self.weight)
+        
+    # 배송 현황 조회
+    def deliCheck(self, today):
+        super().statusCheck(today)
+        
+    
+
+# CU
+class CU(Delivery):
+    def __init__(self, t_name, t_phoneNum, t_address, name, weight, resDay):
+        super().__init__(t_name, t_phoneNum, t_address, name)
+        self.weight = weight
+        self.resDay = resDay
+        self.storeName = 'CU'
+        
+        print(f'{self.storeName} 에서 {self.resDay} 에 {self.name} 물품을 접수하였습니다.')
+    
+    # 무게 
+    def checkWeight(self):
+        super().checkWeight(self.weight)
+        
+    # 배송 현황 조회
+    def deliCheck(self, today):
+        super().statusCheck(today)
+        
+        
 
 # 택배 접수 확인
 delivery_1 = Delivery('Kiki', '010-5678-5678', '서울특별시', '핸드폰')
@@ -101,6 +149,7 @@ print(f'받는 사람 주소: {delivery_1.t_address}\n')
 print(f'예약 번호: {delivery_1.res_num}')
 print(f'운송장번호: {delivery_1.deli_num}')
 print(f'접수 시간: {delivery_1.resDay}\n')
+# print(f'접수 시간: {delivery_1.day}\n')
 
 print(f'배송료: {delivery_1.checkWeight(0.25)}\n')
 
@@ -108,11 +157,19 @@ print(f'예약 번호: {delivery_1.reserveNumCheck(delivery_1.res_num)}\n')
 
 print(f'운송장 번호 조회: {delivery_1.deliveryNumCheck(delivery_1.deli_num)}\n')
 
-# delivery_1.statusCheck(date.today())
+# 현재 날짜, 시간
+# print(datetime.now())
+delivery_1.statusCheck(datetime.now())
 
+print()
+gs = GS25('몽몽', '010-1111-1111', '서울', '컵', 2, datetime(2023, 10, 29))
+gs.checkWeight()
+gs.deliCheck(datetime.now())
 
+print()
 
-
-
+cu = CU('Koko', '010-1234-5678', '서울', '책', 0.3, datetime(2023, 10, 31))
+cu.checkWeight()
+cu.deliCheck(datetime.now())
 
 
